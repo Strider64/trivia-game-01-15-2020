4487cf149bb94cf052bc0b8987487a92558348ef
John Pepp <jrpepp@pepster.com>
2020-01-24 07:26:41 -0500

    add offline error routine

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -12,68 +12,121 @@ const d = document;
 
 const buttonContainer = d.querySelector('#buttonContainer');
 const question = d.querySelector('#question');
+const triviaLabel = d.querySelector('#triviaLabel');
 const next = d.querySelector('#next');
 const points = 100;
 const scoreText = d.querySelector('#score');
+const dSec = 20; // Countdown Clock for questions:
 
 var gameIndex = 0,
         gameData = null,
+        timer = null,
         score = 0,
+        total = 0,
         answeredRight = 0,
         answeredWrong = 0;
 
 const buttons = document.querySelectorAll(".answerButton");
 const mainGame = d.querySelector('#mainGame');
 
-const calcPercent = (correct, wrong) => {
-    var average = (correct / (correct + wrong)) * 100;
+
+/*
+ * Countdown Timer For Triva Game
+ */
+const startTimer = (dSec) => {
+    var seconds = dSec;
+    const userAnswer = 5, correct = 1;
+    const newClock = d.querySelector('#clock');
+
+    newClock.style['color'] = 'white';
+    newClock.textContent = ((seconds < 10) ? '0' + seconds : seconds);
+    const countdown = () => {
+        if (seconds === 0) {
+            clearTimeout(timer);
+            newClock.style['color'] = "red";
+            newClock.textContent = "00";
+            scoringFcn(userAnswer, correct);
+            highlightFCN(userAnswer, correct);
+            disableListeners();
+            next.addEventListener('click', removeQuiz, false);
+        } else {
+            newClock.textContent = ((seconds < 10) ? '0' + seconds : seconds);
+            seconds--;
+        }
+    };
+
+
+
+
+    timer = setInterval(countdown, 1000);
+
+};
+
+const stopTimer = () => {
+    clearInterval(timer);
+};
+
+const highlightFCN = (userAnswer, correct) => {
+
+    const highlights = d.querySelectorAll('.answerButton');
+    highlights.forEach(answer => {
+        /*
+         * Highlight Answers Function
+         */
+        if (userAnswer === correct && userAnswer === parseInt(answer.getAttribute('data-correct'))) {
+            answer.style["background-color"] = 'green';
+        }
+        if (userAnswer !== correct && userAnswer === parseInt(answer.getAttribute('data-correct'))) {
+            answer.style['background-color'] = 'red';
+        }
+        if (userAnswer === 5) {
+            answer.style['background-color'] = 'red';
+        }
+    });
+};
+const disableListeners = () => {
+    const myButtons = d.querySelectorAll('.answerButton');
+    myButtons.forEach(answer => {
+        answer.removeEventListener('click', clickHandler, false);
+    });
+};
+
+const calcPercent = (correct, total) => {
+    var average = (correct / total) * 100;
     d.getElementById('percent').textContent = average.toFixed(0) + "%";
+};
+
+const scoringFcn = (userAnswer, correct) => {
+    if (userAnswer === correct) {
+        score += points;
+        answeredRight++;
+        scoreText.textContent = "Score " + score + " Points";
+    } else {
+        score = score - (points / 2);
+        answeredWrong++;
+        scoreText.textContent = "Score " + score + " Points";
+    }
+    total++;
 };
 
 const clickHandler = (e) => {
     e.preventDefault();
+    stopTimer();
 
-    const myButtons = d.querySelectorAll('.answerButton');
     const correct = gameData[gameIndex].correct;
     const userAnswer = parseInt(e.target.getAttribute('data-correct'));
 
-    var right = true;
 
-    if (userAnswer !== correct) {
-        right = false;
-        score = score - (points/2);
-        answeredWrong ++;
-        scoreText.textContent = "Score " + score + " Points";
-    } else {
-        score += points;
-        answeredRight++;
-        scoreText.textContent = "Score " + score + " Points";
-    }
+    scoringFcn(userAnswer, correct);
+    calcPercent(answeredRight, total);
     
-    calcPercent(answeredRight, answeredWrong);
+    highlightFCN(userAnswer, correct);
 
-    /*
-     * Remove buttons addListeners, so users can't click
-     * more than once.
-     */
-    myButtons.forEach(answer => {
-        /*
-         * Figure out score, percentage and highlight if it is right (green)
-         * or wrong (red)
-         */
-        if (!right && parseInt(answer.getAttribute('data-correct')) === userAnswer) {
-            answer.style["background-color"] = 'red';
-        } else if (right && parseInt(answer.getAttribute('data-correct')) === userAnswer) {
-            answer.style['background-color'] = 'green';
-        }
-        answer.removeEventListener('click', clickHandler, false);
-    });
+    disableListeners();
 
     next.addEventListener('click', removeQuiz, false);
 
 };
-
-
 
 
 const removeQuiz = () => {
@@ -90,6 +143,7 @@ const removeQuiz = () => {
 };
 
 const createQuiz = (gameData) => {
+    startTimer(dSec);
     buttonContainer.setAttribute('data-correct', gameData.correct);
     question.textContent = gameData.question;
 
@@ -108,33 +162,57 @@ const createQuiz = (gameData) => {
     });
 };
 
-
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 const quizUISuccess = function (parsedData) {
 
     mainGame.style.display = 'block';
+
     d.querySelector('#category').removeEventListener('change', selectCat);
-
+    d.querySelector('#category').style.display = 'none';
+    triviaLabel.textContent = capitalizeFirstLetter(parsedData[gameIndex].category);
     gameData = parsedData.sort(() => Math.random() - .5);
-
+    //console.log(gameData);
     createQuiz(gameData[gameIndex]);
-
-
 
 };
 
 const quizUIError = function (error) {
     console.log("Database Table did not load", error);
 
-//    gameData = [
-//        {
-//            id: 1,
-//            category: "movie",
-//            question: "What actor from the movie \"Dead Poets Society\" plays Dr. James Wilson on the TV show ",
-//            answers: ["Ethan Hawke", "Robert Sean Leonard", "James Waterston"],
-//            correct: 2
-//        }
-//    ];
+    gameData = [
+        {
+            id: 1,
+            question: "What actor from the movie \"Dead Poets Society\" plays Dr. James Wilson on the TV show ",
+            correct: 2,
+            category: "movie",
+            answers: ["Ethan Hawke", "Robert Sean Leonard", "James Waterston"]
+
+        },
+        {
+            id: 55,
+            question: "Who has won the most Oscars for Best Actress?",
+            correct: 2,
+            category: "movie",
+            answers: ["Meryl Streep", "Katharine Hepburn", "Audrey Hepburn", "Jane Fonda"]
+        },
+        {
+            id: 9,
+            question: "Who played Jor-El in \"Superman (1978)\"?",
+            correct: 4,
+            category: "movie",
+            answers: ["Glenn Ford", "Ned Beatty", "Christopher Reed", "Marlon Brando"]
+        }
+    ];
+    
+    //console.log(gameData);
+        mainGame.style.display = 'block';
+
+    d.querySelector('#category').removeEventListener('change', selectCat);
+    d.querySelector('#category').style.display = 'none';
+    createQuiz(gameData[gameIndex]);
 };
 
 /*
